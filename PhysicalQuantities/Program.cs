0e1f418a0c0cf63a4bffce027c4d87acc2b2d43d
KorzunK <megaflex2012@gmail.com>
2015-10-07 13:23:23 +0300

    Update Program.cs

--- PhysicalQuantities/Program.cs
+++ PhysicalQuantities/Program.cs
@@ -9,12 +9,27 @@ namespace PhysicalQuantities
     {
         static void Main(string[] args)
         {
-            Length k = new Length(200, "км");
-            Length g = new Length(145, "км");
-            Weight n = new Weight(21, "кг");
+            Unit Speed = new Unit(45, Quantities.Value.Speed.ToString());
+            Unit S = new Unit(45, Quantities.Value.Speed.ToString());
+            Unit Length = new Unit(200, Quantities.Value.Length.ToString());
+            Unit l = new Unit(200, Quantities.Value.Length.ToString());
+            Unit Time = new Unit(63, Quantities.Value.Time.ToString());
+            Unit T = new Unit(63, Quantities.Value.Time.ToString());
 
-            Length sum = k + g;
-            Console.WriteLine("Cevvf = {0}",sum.ToString());
+
+            Quantities.list.Add("Метр(ов)");
+            Quantities.list.Add("Секунд(ы)");
+            Quantities.list.Add("Mетры(ов) в секунду");
+      
+
+            Unit sum = Time + T;
+            Console.WriteLine("Ответ = {0}", sum.ToString());
+
+            Unit dev  = Speed / S;
+            Console.WriteLine("Ответ = {0}", dev.ToString());
+
+            Unit tm = Length / Speed;
+            Console.WriteLine("Ответ = {0}", tm.ToString());
 
             Console.ReadLine();
         }
